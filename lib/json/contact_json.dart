228b0a99355a14277d4ff103902a31449688cffc
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-08 11:50:42 -0300

    Atualização

---
+++ lib/json/contact_json.dart
@@ -0,0 +1,75 @@
+const List contact_data = [
+  {
+    "img":
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    "name": "Marcio Soares",
+    "is_online" : true,
+    "seen" : ""
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    "name": "Monica Ferreira",
+    "is_online" : true,
+    "seen" : ""
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Carlos Diego",
+     "is_online" : true,
+    "seen" : ""
+  },
+  
+  {
+    "img":
+        "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Paulo Feiras",
+     "is_online" : false,
+    "seen" : "visto pela última vez agora"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    "name": "Bem Freitas",
+    "is_online" : false,
+    "seen" : "visto pela última vez agora"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Antonio Soares",
+    "is_online" : false,
+    "seen" : "visto pela última vez agora"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Beto Frontes",
+    "is_online" : false,
+    "seen" : "visto pela última vez 1 minuto atrás"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1619895862022-09114b41f16f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    "name": "Ana Clara",
+    "is_online" : false,
+    "seen" : "visto pela última vez 1 minuto atrás"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1520155707862-5b32817388d6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Foto Sushi",
+    "is_online" : false,
+    "seen" : "visto pela última vez 2 minuto atrás"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Soares F.",
+    "is_online" : false,
+    "seen" : "visto pela última vez 2 minuto atrás"
+  },
+  
+  
+];
---
+++ lib/json/setting_json.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+import 'package:line_icons/line_icons.dart';
+
+List setting_section_one = [
+  {"icon": Icons.bookmark, "text": "Mensagens salvas", "color": Colors.blue[700]},
+  {
+    "icon": FontAwesome.phone,
+    "text": "Chamada recente",
+    "color": Colors.green
+  },
+  {
+    "icon": MaterialIcons.devices,
+    "text": "Dispositivos",
+    "color": Color(0xFFFCAA26)
+  },
+  {
+    "icon": Icons.folder,
+    "text": "Pastas de bate-papo",
+    "color": Colors.cyan
+  },
+];
+
+List setting_section_two = [
+  {"icon": Icons.notifications, "text": "Notificações e sons", "color": Colors.redAccent},
+  {"icon": Icons.lock, "text": "Privacidade e segurança", "color": Colors.grey},
+  {
+    "icon": LineIcons.database,
+    "text": "Data and Storage",
+    "color": Colors.green
+  },
+  {
+    "icon": Icons.dark_mode,
+    "text": "Aparência",
+    "color": Colors.cyan
+  },
+  {
+    "icon": Icons.language,
+    "text": "Linguagem",
+    "color": Colors.purple
+  },
+  {
+    "icon": Icons.sticky_note_2,
+    "text": "Adesivos e emojis",
+    "color": Colors.yellow[800]
+  },
+];
+
+List setting_section_three = [
+  {"icon": Icons.chat, "text": "Ask a Question", "color": Colors.yellow[800]},
+  {
+    "icon": Icons.info,
+    "text": "Telegram FAQ",
+    "color": Colors.cyan
+  },
+  {
+    "icon": MaterialIcons.lightbulb_outline,
+    "text": "Recursos do telegrama",
+    "color": Color(0xFFFCAA26)
+  },
+];
